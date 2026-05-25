a232f218ee37f4145e2761816d2d9c8d881fedd3
Zexdestlny <1016050372@qq.com>
2026-05-25 17:43:43 +0800

    PWA fitness checkin deploy

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'shili-fitness-v1';
+
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request).then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      });
+      return cached || fetched;
+    })
+  );
+});
